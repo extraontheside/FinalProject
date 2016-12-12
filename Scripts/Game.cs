e4fae6398f311bb11577806f1ae41d52f57f4635
extraontheside <extraontheside@hotmail.com>
2016-12-12 14:59:25 -0700

    commit 11 - game

---
+++ Scripts/Game.cs
@@ -0,0 +1,11 @@
+using System;
+public class Game
+{
+    public satic Action StartGame;
+    public static bool canPlay = true;
+    public Game()
+    {
+        Health.power = 100;
+        
+    }
+}
