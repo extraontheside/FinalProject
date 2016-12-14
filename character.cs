5c0029038268fa245e580ba27d51a8d8dbe337b6
extraontheside <extraontheside@hotmail.com>
2016-12-13 19:57:07 -0700

    commit 19

---
+++ character.cs
@@ -0,0 +1,13 @@
+using system;
+public class character
+{
+    private int health;
+    private int strength;
+    private int dexterity;
+
+    public character(){
+        Health = 100;
+        strength = 20;
+        dexterity = 10;
+    } 
+}
